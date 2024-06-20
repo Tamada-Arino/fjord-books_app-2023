dde1e8e9f49b40b9300cc4a308cbb1d858866ff5
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-20 16:53:38 +0900

    userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_POST_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :introduction, length: { maximum: 200 }
+  validates :post_code, format: { with: VALID_POST_CODE_REGEX }, allow_blank: true
 end
