d47f51aab7cba4495e26c5b855017b09cc57ef63
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-20 14:04:45 +0900

    Userに名前と郵便番号・住所カラム追加

--- db/migrate/20240620045443_devise_create_users.rb
+++ db/migrate/20240620045443_devise_create_users.rb
@@ -7,6 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## 追加カラム
+      t.string :name, null: false
+      t.string :post_code
+      t.string :address
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
