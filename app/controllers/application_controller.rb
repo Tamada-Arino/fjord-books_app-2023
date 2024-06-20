fd921a2cf8ac98e7f3669a2a39e7fd9429bf9277
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-20 16:44:19 +0900

    新規登録時に自己紹介が記入できるよう変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :post_code, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :post_code, :address, :introduction])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :post_code, :address, :introduction])
   end
 end
