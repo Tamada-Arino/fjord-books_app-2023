8eed2624768b644bacc95078364f8b365bba0798
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-26 17:36:35 +0900

    コメントコントローラーにアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
+
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
@@ -10,7 +12,28 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
