779d71247c0e31888ef1a9619f02b123297a0cea
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-26 17:33:05 +0900

    コメント作成失敗時に詳細画面に戻る設定を追加

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -6,4 +6,9 @@ class Reports::CommentsController < CommentsController
   def set_commentable
     @commentable = Report.find(params[:report_id])
   end
+
+  def render_commentable_show
+    @book = @commentable
+    render 'books/show'
+  end
 end
