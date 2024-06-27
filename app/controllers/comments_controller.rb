176f85a944568e28f747510201788a5c302e7f76
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-27 12:02:13 +0900

    commentsコントローラーの不要な記述削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[destroy]
-  before_action :correct_user?, only: %i[destroy]
-
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
@@ -16,6 +13,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    redirect_to root_path, alert: t('views.common.unauthorized') unless @comment.user == current_user
     @comment.destroy
 
     redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
@@ -23,15 +22,7 @@ class CommentsController < ApplicationController
 
   private
 
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
   def comment_params
     params.require(:comment).permit(:content)
-  end
-
-  def correct_user?
-    redirect_to root_path, alert: t('views.common.unauthorized') unless @comment.user == current_user
   end
 end
