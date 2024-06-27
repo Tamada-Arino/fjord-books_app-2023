676a1f7ca9b5806fb02192dfc23ebe7da0b5690e
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-27 11:18:36 +0900

    ユーザーの名前がなければメールアドレスを表示する設定追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
   def current_user_name(user)
     [user.name, user.email].find(&:present?)
   end
+
+  def user_name_or_email(user)
+    (user.name.nil? || user.name == "") ? user.email : user.name
+  end
 end
