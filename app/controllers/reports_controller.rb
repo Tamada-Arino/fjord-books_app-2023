2662e834ec918b7416195b4e827b72d89b64b87d
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-28 18:02:37 +0900

    rubocopに従って修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_user_reports, only: %i(edit update delete)
+  before_action :set_user_reports, only: %i[edit update destroy]
 
   def index
     @reports = Report.order(:id)
