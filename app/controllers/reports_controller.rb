88fee37b76d2344e94f327582105f230d00a0b9d
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-28 14:52:25 +0900

    日報のユーザー登録を変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,6 +21,7 @@ class ReportsController < ApplicationController
 
   def create
     @report = Report.new(report_params)
+    @report.user = current_user
 
     respond_to do |format|
       if @report.save
